9f77dee5d5a4d38a05283cd158dc8ab9f2dd3258
spectocr <highstakes22@gmail.com>
2021-05-08 16:06:29 -0400

    fixes

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,8 +35,8 @@ var loadTasks = function() {
   $.each(tasks, function(list, arr) {
     console.log(list, arr);
     // then loop over sub-array
-    arr.forEach(function(tasks) {
-      createTask(tasks.text, tasks.date, list);
+    arr.forEach(function(task) {
+      createTask(task.text, task.date, list);
     });
   });
 };
@@ -44,56 +44,6 @@ var loadTasks = function() {
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
-
-//add lesson 5.1.6 content.
-$(".list-group").on("click", "p", function() {
-  console.log(this);
-});
-
-//5.1.6 more
-$(".list-group").on("click", "p", function() {
-  var text = $(this)
-  .text()
-  .trim();
-  var textInput = $("<textarea>")
-  .addClass("form-control")
-  .val(text);
-  $(this).replaceWith(textInput);
-  textInput.trigger("focus");
-
-  console.log(text);
-
-
-// get the textarea's current value/text
-var text = $(this)
-  .val()
-  .trim();
-
-// get the parent ul's id attribute
-var status = $(this)
-  .closest(".list-group")
-  .attr("id")
-  .replace("list-", "");
-console.log("this is " + this);
-// get the task's position in the list of other li elements
-var index = $(this)
-  .closest(".list-group-item")
-  .index();
-
- 
-
-// recreate p element
-var taskP = $("<p>")
-  .addClass("m-1")
-  .text(text);
-
-// replace textarea with p element
-$(this).replaceWith(taskP);
-
-tasks[status][index].text = text;
-saveTasks();
-
-});
 
 
 
@@ -140,6 +90,48 @@ $("#remove-tasks").on("click", function() {
   }
   saveTasks();
 });
+//begin 5.1.6 coding add.
+$(".list-group").on("click", "p", function() {
+  var text = $(this)
+  .text()
+  .trim();
+  
+  var textInput = $("<textarea>")
+  .addClass("form-control")
+  .val(text);
+  $(this).replaceWith(textInput);
+  textInput.trigger("focus");
+});
+
+$(".list-group").on("blur", "textarea", function() {
+// get the textarea's current value/text
+var text = $(this)
+  .val()
+  .trim();
+console.log(this);
+// get the parent ul's id attribute
+var status = $(this)
+  .closest(".list-group")
+  .attr("id")
+  .replace("list-", "");
+
+// get the task's position in the list of other li elements
+var index = $(this)
+  .closest(".list-group-item")
+  .index();
+
+  // recreate p element
+var taskP = $("<p>")
+.addClass("m-1")
+.text(text);
+
+// replace textarea with p element
+$(this).replaceWith(taskP);
+
+tasks[status][index].text = text;
+saveTasks();
+});
+
 
 // load tasks for the first time
 loadTasks();
